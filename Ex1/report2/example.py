05f52a69df92ee5c99c49424300575de94cb03fb
Anders Sildnes <andsild@gmail.com>
2014-10-01 01:49:40 +0200

    go

--- Ex1/report2/example.py
+++ Ex1/report2/example.py
@@ -4,4 +4,4 @@ symvars = symbols(...variables)
 lambdafunc =lambdify(symvars, Ne(*symvars))
 for symv,v in zip(symvars, variables):
     sym_to_variable[symv] = int(v)
-c = Constraint(lambdafunc, (symsvars,variables), D, CNET)
+c = Constraint(lambdafunc, (symsvars,variables), sym_to_variable, CNET)
